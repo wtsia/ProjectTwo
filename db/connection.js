239f903b589b11bb10d40b1be25836da27ba29e1
Winston Tsia <winstontsia@gmail.com>
2019-10-11 09:15:19 -0700

    checking heroku

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
 } else {
-  mongoURI = "mongodb://localhost/Memes";
+  mongoURI = "mongodb://localhost/TheDailyMeme";
 }
 
 // connect to the database, with the imported mongoose instance
